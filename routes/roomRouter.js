4573c838efafa6887375f00e90a9647828ea19f4
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-07 20:33:26 +0700

    07-08-2023

--- routes/roomRouter.js
+++ routes/roomRouter.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { createRoom } from "../controllers/room.js";
 // import { createRoom, deleteRoom, getRooms } from "../controllers/room.js";
-// import auth from '../middleware/auth.js';
+import auth from '../middleware/auth.js';
 
 const roomRouter = Router();
-// roomRouter.post('/', auth, createRoom);
-roomRouter.post("/", createRoom);
+roomRouter.post('/', auth, createRoom);
 // roomRouter.get('/', getRooms);
 // roomRouter.delete('/:roomId', deleteRoom);
 
