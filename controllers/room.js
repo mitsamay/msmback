337ebbf36add02301f90051deac234d5c74c469a
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-13 16:34:28 +0700

    13-08-2023

--- controllers/room.js
+++ controllers/room.js
@@ -1,12 +1,12 @@
-// import Room from '../models/Room.js';
-// import tryCatch from './utils/tryCatch.js';
+import Room from '../models/Room.js';
+import tryCatch from './utils/tryCatch.js';
 
-// export const createRoom = tryCatch(async (req, res) => {
-//   const { id: uid, name: uName, photoURL: uPhoto } = req.user;
-//   const newRoom = new Room({ ...req.body, uid, uName, uPhoto });
-//   await newRoom.save();
-//   res.status(201).json({ success: true, result: newRoom });
-// });
+export const createRoom = tryCatch(async (req, res) => {
+  const { id: uid, name: uName, photoURL: uPhoto } = req.user;
+  const newRoom = new Room({ ...req.body, uid, uName, uPhoto });
+  await newRoom.save();
+  res.status(201).json({ success: true, result: newRoom });
+});
 
 // export const getRooms = tryCatch(async (req, res) => {
 //   const rooms = await Room.find().sort({ _id: -1 });
@@ -18,9 +18,11 @@
 //   res.status(200).json({ success: true, result: { _id } });
 // });
 
-export const createRoom = async (req, res) => {
-  //testing resource access
-  res
-    .status(201)
-    .json({ success: true, result: { id: 123, title: "test room" } });
-};
+
+// Test ທົດສອບເບື້ອງຕົ້ນ
+// export const createRoom = async (req, res) => {
+//   //testing resource access
+//   res
+//     .status(201)
+//     .json({ success: true, result: { id: 123, title: "test room" } });
+// };
